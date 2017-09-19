be18ccbf18f21237a97e8c35656fa74d03a0cc55
Luis Correia <buga@loide.net>
2017-09-19 22:39:40 +0100

    Bora inventar cenas e escrever à parva para o buzio, parte 3.2

--- pgm/i2c.cpp
+++ pgm/i2c.cpp
@@ -32,7 +32,7 @@ void CloseI2C()
 
 void SetI2CAddr(uint8_t address)
 {
-    printf("Working with device %02x.\n",address);
+    printf("Working with device %02x\n",address);
 
     if (ioctl(g_i2cFile, I2C_SLAVE, address) < 0)
     {
@@ -43,11 +43,11 @@ void SetI2CAddr(uint8_t address)
 
 bool WriteBytesToAddr(uint8_t reg, uint8_t* values, uint8_t len)
 {
-    printf("Writing %i bytes to %02x.\n",len,reg);
+    printf("Writing %i bytes to %02x\n",len,reg);
 
     if (write(g_i2cFile, values, len) != len)
     {
-        printf("Failed to write to the i2c bus.\n");
+        printf("Failed to write to the i2c bus\n");
         return 0;
     }
     else
@@ -59,12 +59,12 @@ bool WriteBytesToAddr(uint8_t reg, uint8_t* values, uint8_t len)
 
 bool ReadBytesFromAddr(uint8_t reg, uint8_t* dest, uint8_t len)
 {
-    printf("Reading %i bytes from %02x.\n",len,reg);
+    printf("Reading %i bytes from %02x\n",len,reg);
 
     if (read(g_i2cFile, dest, len) != len)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
     {
         //ERROR HANDLING: i2c transaction failed
-        printf("Failed to read from the i2c bus.\n");
+        printf("Failed to read from the i2c bus\n");
         return 0;
     }
     else
@@ -78,13 +78,13 @@ uint8_t ReadReg(uint8_t reg)
     uint8_t result;
     ReadBytesFromAddr(reg, &result, 1);
 
-    printf("Read %02x from %02x",result,reg);
+    printf("Read %02x from %02x\n",result,reg);
 
     return result;
 }
 
 bool WriteReg(uint8_t reg, uint8_t value)
 {
-    printf("Writing %02x to %02x",value,reg);
+    printf("Writing %02x to %02x\n",value,reg);
     return WriteBytesToAddr(reg, &value, 1);
 }

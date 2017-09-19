0d83d81fa6fdc584211dab0c4205d6144db29553
Luis Correia <buga@loide.net>
2017-09-19 22:35:33 +0100

    Bora inventar cenas e escrever à parva para o buzio, parte 3.1

--- pgm/i2c.cpp
+++ pgm/i2c.cpp
@@ -32,7 +32,7 @@ void CloseI2C()
 
 void SetI2CAddr(uint8_t address)
 {
-    printf("Working with device %i.\n",address);
+    printf("Working with device %02x.\n",address);
 
     if (ioctl(g_i2cFile, I2C_SLAVE, address) < 0)
     {
@@ -43,7 +43,7 @@ void SetI2CAddr(uint8_t address)
 
 bool WriteBytesToAddr(uint8_t reg, uint8_t* values, uint8_t len)
 {
-    printf("Writing %i bytes to %i.\n",len,reg);
+    printf("Writing %i bytes to %02x.\n",len,reg);
 
     if (write(g_i2cFile, values, len) != len)
     {
@@ -56,14 +56,10 @@ bool WriteBytesToAddr(uint8_t reg, uint8_t* values, uint8_t len)
     }
 }
 
-bool WriteReg(uint8_t reg, uint8_t value)
-{
-    return WriteBytesToAddr(reg, &value, 1);
-}
 
 bool ReadBytesFromAddr(uint8_t reg, uint8_t* dest, uint8_t len)
 {
-    printf("Reading %i bytes from %i.\n",len,reg);
+    printf("Reading %i bytes from %02x.\n",len,reg);
 
     if (read(g_i2cFile, dest, len) != len)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
     {
@@ -81,6 +77,14 @@ uint8_t ReadReg(uint8_t reg)
 {
     uint8_t result;
     ReadBytesFromAddr(reg, &result, 1);
+
+    printf("Read %02x from %02x",result,reg);
+
     return result;
 }
 
+bool WriteReg(uint8_t reg, uint8_t value)
+{
+    printf("Writing %02x to %02x",value,reg);
+    return WriteBytesToAddr(reg, &value, 1);
+}

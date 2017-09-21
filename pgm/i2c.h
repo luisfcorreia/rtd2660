76db6544787d9c32f37de00c82d3351d20c1eebe
Luis Correia <buga@loide.net>
2017-09-22 00:20:32 +0100

    Save full project

--- pgm/i2c.h
+++ pgm/i2c.h
@@ -11,7 +11,3 @@ bool WriteReg(uint8_t reg, uint8_t value);
 uint8_t ReadReg(uint8_t reg);
 bool ReadBytesFromAddr(uint8_t reg, uint8_t* dest, uint8_t len);
 bool WriteBytesToAddr(uint8_t reg, uint8_t* values, uint8_t len);
-
-#define BYTE uint8_t
-#define WORD uint16_t
-#define LONG uint32_t
